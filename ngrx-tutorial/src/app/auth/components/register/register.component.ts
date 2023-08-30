d1c179f43e3f5f287e8c6f5845e7012484080dea
soggiuinsulab <alessio.soggiu@insulab.ai>
2023-08-30 17:35:13 +0200

    before removing selectors

---
+++ ngrx-tutorial/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterLink } from '@angular/router';
+import { selectIsSubmitting } from '../../store/selectors';
+import { AuthStateInterface } from '../../types/authState.interface';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, CommonModule]
+})
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+  isSubmitting$ = this.store.select(selectIsSubmitting)
+
+  constructor(private fb: FormBuilder, private store: Store<{auth: AuthStateInterface}>) {}
+
+  onSubmit() {
+    console.log(this.form.getRawValue())
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue()
+    }
+    this.store.dispatch(register({ request }))
+  }
+}
---
+++ ngrx-tutorial/src/main.ts
@@ -0,0 +1,26 @@
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import { appRoutes } from "./app/app.routes";
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { isDevMode } from '@angular/core';
+import { authFeatureKey, authReducer } from "./app/auth/store/reducers";
+
+bootstrapApplication(
+  AppComponent,
+  {
+    providers: [
+      provideRouter(appRoutes),
+      provideStore(),
+      provideState(authFeatureKey, authReducer),
+      provideStoreDevtools({
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: false,
+        traceLimit: 75
+      })
+    ]
+  }
+)
